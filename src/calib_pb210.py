114f65e1dc34175f36103d02715520f19984305c
sxu0 <sxu0726@gmail.com>
2022-03-28 20:15:22 -0400

    format file

--- src/calib_pb210.py
+++ src/calib_pb210.py
@@ -7,10 +7,12 @@ from scipy.optimize import curve_fit
 
 
 def gaussian(x, height, centre, std):
-    return height * np.exp(-(x - centre)**2 / (2 * std ** 2))
+    return height * np.exp(-((x - centre) ** 2) / (2 * std ** 2))
+
 
 def line(x, slope, intercept):
     return slope * x + intercept
+
 
 def fit_points(x_data, y_data, func, guess):
     fitted, err_cov = curve_fit(func, x_data, y_data, p0=guess)
@@ -30,71 +32,122 @@ if __name__ == "__main__":
     channels = np.arange(0, 2048)
 
     ## Pb-210 peak 1
-    pb210_peak1_fit, pb210_peak1_err = fit_points(channels[850:882], pb210_data[850:882], gaussian, [34, 866, 20])
+    pb210_peak1_fit, pb210_peak1_err = fit_points(
+        channels[850:882], pb210_data[850:882], gaussian, [34, 866, 20]
+    )
     pb210_peak1_fit_x = np.arange(800, 932)
-    pb210_peak1_fit_y = gaussian(pb210_peak1_fit_x, pb210_peak1_fit[0], pb210_peak1_fit[1], pb210_peak1_fit[2])
+    pb210_peak1_fit_y = gaussian(
+        pb210_peak1_fit_x, pb210_peak1_fit[0], pb210_peak1_fit[1], pb210_peak1_fit[2]
+    )
     pb210_peak1_centroid = round(pb210_peak1_fit[1], 2)
     plt.figure()
-    plt.plot(channels[850:882], pb210_data[850:882], 'x', label="data")
+    plt.plot(channels[850:882], pb210_data[850:882], "x", label="data")
     plt.plot(pb210_peak1_fit_x, pb210_peak1_fit_y, label="fit")
     plt.title("Pb-210 peak centred around channel " + str(pb210_peak1_centroid))
     plt.xlabel("Channel")
     plt.ylabel("Count")
-    plt.text(175, 385, "centroid:   " + str(pb210_peak1_centroid), ha='center', va='center', transform=None)
+    plt.text(
+        175,
+        385,
+        "centroid:   " + str(pb210_peak1_centroid),
+        ha="center",
+        va="center",
+        transform=None,
+    )
     plt.legend()
     # plt.savefig(output_path / "20220317_pb210_peak1.png")
     plt.close()
 
     ## Pb-210 peak 2
-    pb210_peak2_fit, pb210_peak2_err = fit_points(channels[998:1023], pb210_data[998:1023], gaussian, [10, 1010, 11])
-    pb210_peak2_fit_x = np.arange(998-50, 1023+50)
-    pb210_peak2_fit_y = gaussian(pb210_peak2_fit_x, pb210_peak2_fit[0], pb210_peak2_fit[1], pb210_peak2_fit[2])
+    pb210_peak2_fit, pb210_peak2_err = fit_points(
+        channels[998:1023], pb210_data[998:1023], gaussian, [10, 1010, 11]
+    )
+    pb210_peak2_fit_x = np.arange(998 - 50, 1023 + 50)
+    pb210_peak2_fit_y = gaussian(
+        pb210_peak2_fit_x, pb210_peak2_fit[0], pb210_peak2_fit[1], pb210_peak2_fit[2]
+    )
     pb210_peak2_centroid = round(pb210_peak2_fit[1], 2)
     plt.figure()
-    plt.plot(channels[998:1023], pb210_data[998:1023], 'x', label="data")
+    plt.plot(channels[998:1023], pb210_data[998:1023], "x", label="data")
     plt.plot(pb210_peak2_fit_x, pb210_peak2_fit_y, label="fit")
     plt.title("Pb-210 peak centred around channel " + str(pb210_peak2_centroid))
     plt.xlabel("Channel")
     plt.ylabel("Count")
-    plt.text(175, 385, "centroid:   " + str(pb210_peak2_centroid), ha='center', va='center', transform=None)
+    plt.text(
+        175,
+        385,
+        "centroid:   " + str(pb210_peak2_centroid),
+        ha="center",
+        va="center",
+        transform=None,
+    )
     plt.legend()
     # plt.savefig(output_path / "20220317_pb210_peak2.png")
     plt.close()
 
     ## Pb-210 peak 3
-    pb210_peak3_fit, pb210_peak3_err = fit_points(channels[1024:1068], pb210_data[1024:1068], gaussian, [18, 1046, 22])
-    pb210_peak3_fit_x = np.arange(1024-50, 1068+50)
-    pb210_peak3_fit_y = gaussian(pb210_peak3_fit_x, pb210_peak3_fit[0], pb210_peak3_fit[1], pb210_peak3_fit[2])
+    pb210_peak3_fit, pb210_peak3_err = fit_points(
+        channels[1024:1068], pb210_data[1024:1068], gaussian, [18, 1046, 22]
+    )
+    pb210_peak3_fit_x = np.arange(1024 - 50, 1068 + 50)
+    pb210_peak3_fit_y = gaussian(
+        pb210_peak3_fit_x, pb210_peak3_fit[0], pb210_peak3_fit[1], pb210_peak3_fit[2]
+    )
     pb210_peak3_centroid = round(pb210_peak3_fit[1], 2)
     plt.figure()
-    plt.plot(channels[1024:1068], pb210_data[1024:1068], 'x', label="data")
+    plt.plot(channels[1024:1068], pb210_data[1024:1068], "x", label="data")
     plt.plot(pb210_peak3_fit_x, pb210_peak3_fit_y, label="fit")
     plt.title("Pb-210 peak centred around channel " + str(pb210_peak3_centroid))
     plt.xlabel("Channel")
     plt.ylabel("Count")
-    plt.text(175, 385, "centroid:   " + str(pb210_peak3_centroid), ha='center', va='center', transform=None)
+    plt.text(
+        175,
+        385,
+        "centroid:   " + str(pb210_peak3_centroid),
+        ha="center",
+        va="center",
+        transform=None,
+    )
     plt.legend()
     # plt.savefig(output_path / "20220317_pb210_peak3.png")
     plt.close()
 
     ## Pb-210 peak 4
-    pb210_peak4_fit, pb210_peak4_err = fit_points(channels[1236:1260], pb210_data[1236:1260], gaussian, [6, 1248, 6])
-    pb210_peak4_fit_x = np.arange(1236-25, 1260+25)
-    pb210_peak4_fit_y = gaussian(pb210_peak4_fit_x, pb210_peak4_fit[0], pb210_peak4_fit[1], pb210_peak4_fit[2])
+    pb210_peak4_fit, pb210_peak4_err = fit_points(
+        channels[1236:1260], pb210_data[1236:1260], gaussian, [6, 1248, 6]
+    )
+    pb210_peak4_fit_x = np.arange(1236 - 25, 1260 + 25)
+    pb210_peak4_fit_y = gaussian(
+        pb210_peak4_fit_x, pb210_peak4_fit[0], pb210_peak4_fit[1], pb210_peak4_fit[2]
+    )
     pb210_peak4_centroid = round(pb210_peak4_fit[1], 2)
     plt.figure()
-    plt.plot(channels[1236:1260], pb210_data[1236:1260], 'x', label="data")
+    plt.plot(channels[1236:1260], pb210_data[1236:1260], "x", label="data")
     plt.plot(pb210_peak4_fit_x, pb210_peak4_fit_y, label="fit")
     plt.title("Pb-210 peak centred around channel " + str(pb210_peak4_centroid))
     plt.xlabel("Channel")
     plt.ylabel("Count")
-    plt.text(175, 385, "centroid:   " + str(pb210_peak4_centroid), ha='center', va='center', transform=None)
+    plt.text(
+        175,
+        385,
+        "centroid:   " + str(pb210_peak4_centroid),
+        ha="center",
+        va="center",
+        transform=None,
+    )
     plt.legend()
     # plt.savefig(output_path / "20220317_pb210_peak4.png")
     plt.close()
 
     ## Pb-210 calibration curve
-    peak_channels = np.array([pb210_peak1_centroid, pb210_peak2_centroid, pb210_peak3_centroid, pb210_peak4_centroid])
+    peak_channels = np.array(
+        [
+            pb210_peak1_centroid,
+            pb210_peak2_centroid,
+            pb210_peak3_centroid,
+            pb210_peak4_centroid,
+        ]
+    )
     test_energies = np.array([10.555, 12.305, 12.618, 15.222])  # from literature table
     calib_guess = [0, 0]
     calib_fit, calib_err = fit_points(peak_channels, test_energies, line, calib_guess)
@@ -103,10 +156,21 @@ if __name__ == "__main__":
     fit_y = line(fit_x, calib_fit[0], calib_fit[1])
     plt.figure()
     plt.plot(fit_x, fit_y)
-    plt.plot(peak_channels, test_energies, '.')
+    plt.plot(peak_channels, test_energies, ".")
     plt.title("Pb-210 Calibration Curve")
     plt.xlabel("Channel $N$")
     plt.ylabel("Energy $E$ (keV)")
-    plt.text(245, 385, "calibration curve: $E=" + str(round(calib_fit[0], 4)) + "N" + str(round(calib_fit[1], 3)) + "$", ha='center', va='center', transform=None)
+    plt.text(
+        245,
+        385,
+        "calibration curve: $E="
+        + str(round(calib_fit[0], 4))
+        + "N"
+        + str(round(calib_fit[1], 3))
+        + "$",
+        ha="center",
+        va="center",
+        transform=None,
+    )
     # plt.savefig(output_path / "20220317_pb210_calib_curve.png")
     plt.close()
