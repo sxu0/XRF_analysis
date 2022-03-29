ad7b009806060ff29705a1ee8571b67d05536fa0
sxu0 <sxu0726@gmail.com>
2022-03-28 20:13:41 -0400

    produce Pb-210 calibration curve using my centroids (2022-03-17 data)

--- src/calibration.py
+++ src/calibration.py
@@ -29,7 +29,7 @@ if __name__ == "__main__":
     pb210_data = read_data("data//20220317_take01_Pb210_1523count.csv")
     channels = np.arange(0, 2048)
 
-    ## peak 1
+    ## Pb-210 peak 1
     pb210_peak1_fit, pb210_peak1_err = fit_points(channels[850:882], pb210_data[850:882], gaussian, [34, 866, 20])
     pb210_peak1_fit_x = np.arange(800, 932)
     pb210_peak1_fit_y = gaussian(pb210_peak1_fit_x, pb210_peak1_fit[0], pb210_peak1_fit[1], pb210_peak1_fit[2])
@@ -43,8 +43,9 @@ if __name__ == "__main__":
     plt.text(175, 385, "centroid:   " + str(pb210_peak1_centroid), ha='center', va='center', transform=None)
     plt.legend()
     # plt.savefig(output_path / "20220317_pb210_peak1.png")
+    plt.close()
 
-    ## peak 2
+    ## Pb-210 peak 2
     pb210_peak2_fit, pb210_peak2_err = fit_points(channels[998:1023], pb210_data[998:1023], gaussian, [10, 1010, 11])
     pb210_peak2_fit_x = np.arange(998-50, 1023+50)
     pb210_peak2_fit_y = gaussian(pb210_peak2_fit_x, pb210_peak2_fit[0], pb210_peak2_fit[1], pb210_peak2_fit[2])
@@ -58,8 +59,9 @@ if __name__ == "__main__":
     plt.text(175, 385, "centroid:   " + str(pb210_peak2_centroid), ha='center', va='center', transform=None)
     plt.legend()
     # plt.savefig(output_path / "20220317_pb210_peak2.png")
+    plt.close()
 
-    ## peak 3
+    ## Pb-210 peak 3
     pb210_peak3_fit, pb210_peak3_err = fit_points(channels[1024:1068], pb210_data[1024:1068], gaussian, [18, 1046, 22])
     pb210_peak3_fit_x = np.arange(1024-50, 1068+50)
     pb210_peak3_fit_y = gaussian(pb210_peak3_fit_x, pb210_peak3_fit[0], pb210_peak3_fit[1], pb210_peak3_fit[2])
@@ -73,8 +75,9 @@ if __name__ == "__main__":
     plt.text(175, 385, "centroid:   " + str(pb210_peak3_centroid), ha='center', va='center', transform=None)
     plt.legend()
     # plt.savefig(output_path / "20220317_pb210_peak3.png")
+    plt.close()
 
-    ## peak 4
+    ## Pb-210 peak 4
     pb210_peak4_fit, pb210_peak4_err = fit_points(channels[1236:1260], pb210_data[1236:1260], gaussian, [6, 1248, 6])
     pb210_peak4_fit_x = np.arange(1236-25, 1260+25)
     pb210_peak4_fit_y = gaussian(pb210_peak4_fit_x, pb210_peak4_fit[0], pb210_peak4_fit[1], pb210_peak4_fit[2])
@@ -88,17 +91,22 @@ if __name__ == "__main__":
     plt.text(175, 385, "centroid:   " + str(pb210_peak4_centroid), ha='center', va='center', transform=None)
     plt.legend()
     # plt.savefig(output_path / "20220317_pb210_peak4.png")
+    plt.close()
 
+    ## Pb-210 calibration curve
+    peak_channels = np.array([pb210_peak1_centroid, pb210_peak2_centroid, pb210_peak3_centroid, pb210_peak4_centroid])
+    test_energies = np.array([10.555, 12.305, 12.618, 15.222])  # from literature table
+    calib_guess = [0, 0]
+    calib_fit, calib_err = fit_points(peak_channels, test_energies, line, calib_guess)
 
-    # peak_channels = np.array([866, 1010, 1046, 1247])
-    # test_energies = np.array([10.555, 12.305, 12.618, 15.222])
-    # calib_guess = [0, 0]
-    # calib_fit, calib_err = fit_points(peak_channels, test_energies, line, calib_guess)
-
-    # fit_x = np.linspace(0, 2047, 2048)
-    # fit_y = line(fit_x, calib_fit[0], calib_fit[1])
-    # plt.figure()
-    # plt.plot(fit_x, fit_y)
-    # plt.plot(peak_channels, test_energies, '.')
-    # plt.show()
-    # print(calib_fit)
+    fit_x = np.arange(700, 1400)
+    fit_y = line(fit_x, calib_fit[0], calib_fit[1])
+    plt.figure()
+    plt.plot(fit_x, fit_y)
+    plt.plot(peak_channels, test_energies, '.')
+    plt.title("Pb-210 Calibration Curve")
+    plt.xlabel("Channel $N$")
+    plt.ylabel("Energy $E$ (keV)")
+    plt.text(245, 385, "calibration curve: $E=" + str(round(calib_fit[0], 4)) + "N" + str(round(calib_fit[1], 3)) + "$", ha='center', va='center', transform=None)
+    # plt.savefig(output_path / "20220317_pb210_calib_curve.png")
+    plt.close()
