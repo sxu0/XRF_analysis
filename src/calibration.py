cefe54c3972d68b29da5d6651f545c19f8d1a059
sxu0 <sxu0726@gmail.com>
2022-04-01 09:19:05 -0400

    add calibration curve function

--- src/calibration.py
+++ src/calibration.py
@@ -39,7 +39,7 @@ def read_data(filename: Path):
         filename (Path): Path to data file (CSV format).
 
     Returns:
-        np.ndarray: 1D array containing counts in each channel
+        np.ndarray[int]: 1D array containing counts in each channel
             (index corresponds to channel number).
     """
     with open(filename, "r") as file:
@@ -71,7 +71,7 @@ def fit_peak(
     Produces a plot.
 
     Args:
-        counts (np.ndarray): Counts seen in each channel.
+        counts (np.ndarray[int]): Counts seen in each channel.
         first_channel (int): Lower bound on channels to include in fit.
         last_channel (int): Upper bound on channels to include in fit.
         guess (List[float]): Guesses for Gaussian parameters, [height, centre, std].
@@ -110,6 +110,52 @@ def fit_peak(
         transform=None,
     )
     plt.legend()
+    if save_fig:
+        plt.savefig(path_save)
+        plt.close()
+    else:
+        plt.show()
+
+
+def calib_curve(
+    peak_centres: np.ndarray,
+    energies: np.ndarray,
+    guess: List[float],
+    sample: str,
+    save_fig: bool = False,
+    path_save: Path = None,
+):
+    """Produce a calibration curve given peak locations and test known energies from literature.
+
+    Args:
+        peak_centres (np.ndarray[float]): Locations of peaks (centre channel).
+        energies (np.ndarray[float]): Energies of each peak, from literature.
+        guess (List[float]): Guesses for [slope, intercept] of calibration line.
+        sample (str): Name of sample used for calibration. Used in plot title.
+        save_fig (bool, optional): Whether to save output plot. Defaults to False.
+        path_save (Path, optional): Path to save output plot. Defaults to None.
+    """
+    calib_fit, calib_err = fit_points(peak_centres, energies, line, guess)
+    fit_x = np.arange()
+    fit_y = line(fit_x, calib_fit[0], calib_fit[1])
+    plt.figure()
+    plt.plot(fit_x, fit_y)
+    plt.plot(peak_centres, energies, ".")
+    plt.title(sample + " Calibration Curve")
+    plt.xlabel("Channel $N$")
+    plt.ylabel("Energy $E$ (keV)")
+    plt.text(
+        245,
+        385,
+        "calibration curve: $E="
+        + str(round(calib_fit[0], 4))
+        + "N"
+        + str(round(calib_fit[1], 3))
+        + "$",
+        ha="center",
+        va="center",
+        transform=None,
+    )
     if save_fig:
         plt.savefig(path_save)
         plt.close()
